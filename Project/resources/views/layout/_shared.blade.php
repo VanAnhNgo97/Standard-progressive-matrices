80dc1c3885b4e5269f4e91b4ae33faaaf4984430
VanAnhNgo97 <vananhngo97@gmail.com>
2018-11-04 10:47:39 +0700

    template
    create new laravel project

---
+++ Project/resources/views/layout/_shared.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Test IQ</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="Lingua project">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap4/bootstrap.min.css')}}">
+<link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" 
+		href="{{asset('plugins/OwlCarousel2-2.2.1/owl.carousel.css')}}">
+<link rel="stylesheet" type="text/css" 
+		href="{{asset('plugins/OwlCarousel2-2.2.1/owl.theme.default.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('plugins/OwlCarousel2-2.2.1/animate.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('css/main_styles.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('css/responsive.css')}}">
+</head>
+<body>
+
+<div class="super_container">
+	{{-- Header --}}
+	@include('layout.header')
+	{{--Content--}}
+	@yield('content')
+	{{-- Footer --}}
+	@include('layout.footer')
+<script src="{{asset('js/jquery-3.2.1.min.js')}}"></script>
+<script src="{{asset('css/bootstrap4/popper.js')}}"></script>
+<script src="{{asset('css/bootstrap4/bootstrap.min.js')}}"></script>
+<script src="{{asset('plugins/OwlCarousel2-2.2.1/owl.carousel.js')}}"></script>
+<script src="{{asset('plugins/easing/easing.js')}}"></script>
+<script src="{{asset('js/custom.js')}}"></script>
+	@yield('js_css')
+</body>
+</html>
