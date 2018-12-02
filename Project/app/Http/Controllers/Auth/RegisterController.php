aac2f260ebfd60f2234059b6de818bc5e1af3161
VanAnhNgo97 <vananhngo97@gmail.com>
2018-12-02 11:16:59 +0700

    login template

--- Project/app/Http/Controllers/Auth/RegisterController.php
+++ Project/app/Http/Controllers/Auth/RegisterController.php
@@ -52,6 +52,8 @@ class RegisterController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
+            'full_name' => 'required|string|max:255',
+            'permission' => 'required|string|max:255',
         ]);
     }
 
@@ -64,9 +66,11 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+            'username' => $data['name'],
             'email' => $data['email'],
+            'full_name' => $data['full_name'],
             'password' => Hash::make($data['password']),
+            'permission' => $data['permission'],
         ]);
     }
 }
--- Project/resources/views/login.blade.php
+++ Project/resources/views/login.blade.php
@@ -1,1 +1,69 @@
-login.blade.php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Login V15</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="csrf-token" content="{{ csrf_token() }}">
+	<!--bootstrap-->
+	<link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap4/bootstrap.min.css')}}">
+	<!--font-awesome-->
+	<link href="{{asset('plugins/font-awesome-4.7.0/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
+	<!--login css-->
+	<link rel="stylesheet" type="text/css" href="{{asset('css/login/util.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('css/login/main.css')}}">
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<div class="login100-form-title" style="background-image: url({{asset('images/bg-01.jpg')}});">
+					<span class="login100-form-title-1">
+						Đăng nhập
+					</span>
+				</div>
+
+				<form class="login100-form validate-form">
+					<div class="wrap-input100 validate-input m-b-26" data-validate="Vui lòng điền nhập tài khoản">
+						<span class="label-input100">Tài khoản</span>
+						<input class="input100" type="text" name="username" placeholder="Tên đăng nhập">
+						<span class="focus-input100"></span>
+					</div>
+
+					<div class="wrap-input100 validate-input m-b-18" data-validate = "Vui lòng nhập mật khẩu">
+						<span class="label-input100">Mật khẩu</span>
+						<input class="input100" type="password" name="pass" placeholder="Mật khẩu">
+						<span class="focus-input100"></span>
+					</div>
+
+					<div class="flex-sb-m w-full p-b-30">
+						<div class="contact100-form-checkbox">
+							<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
+							<label class="label-checkbox100" for="ckb1">
+								Ghi nhớ
+							</label>
+						</div>
+
+						<div>
+							<a href="#" class="txt1">
+								Quên mật khẩu?
+							</a>
+						</div>
+					</div>
+
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+							Đăng nhập
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+<script src="{{asset('js/jquery-3.2.1.min.js')}}"></script>
+<script src="{{asset('css/bootstrap4/popper.js')}}"></script>
+<script src="{{asset('css/bootstrap4/bootstrap.min.js')}}"></script>
+<script src="{{asset('js/login.js')}}"></script>
+</body>
+</html>
